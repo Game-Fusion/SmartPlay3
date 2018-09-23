716445998529774440886bdfccd0f57ea84e93ce
TheMrIron2 <dwelch.appleid@gmail.com>
2018-09-23 14:47:21 +0100

    Cleanup

--- src/startup.lua
+++ src/startup.lua
@@ -6,11 +6,18 @@ function centerSlow(y,string)
   textutils.slowPrint(string,27)
 end
 
+function center(y,string)
+  local w,h = term.getSize()
+  local x = (w/2)-(#string/2)
+  term.setCursorPos(x,y)
+  print(string)
+end
+
 term.setBackgroundColour(colours.grey)
 term.setTextColour(colours.white)
 term.clear()
 centerSlow(3,"Welcome to SmartPlay 3.")
 sleep(0.25)
 centerSlow(4,"It's like music to your ears.")
-sleep(2.25)
+sleep(2)
 shell.run("SP3/Desktop")
